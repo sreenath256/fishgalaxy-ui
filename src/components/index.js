55261e6b73f8d7fc0a63d1a48b2ebbe3dd8e46c0
rashid-090 <rashidpk090@gmail.com>
2025-06-30 12:10:35 +0530

    donee

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,6 @@ import Footer from "./footer";
 import Banner from "./Banner";
 import CategoriesCarousel from './products/allCategories';
 import Bestoffers from './products/Bestoffers';
-import NewArrivlas from "./products/Newarrivals";
+import NewArrivlas from "./products/newArrivals";
 
 export{Header, Footer, Banner, CategoriesCarousel, Bestoffers,NewArrivlas}
